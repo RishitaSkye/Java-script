9506db21f933a4e234b8a4b877007e064f5cacf0
rishita das <er.rishitadas@gmail.com>
2025-02-14 02:40:00 +0530

    Variables

---
+++ variables.js
@@ -0,0 +1,20 @@
+const account_id = 144553 //can't change
+let account_Email = "trisha@google.com"  //can change  //variable  declaration
+var account_Password ="52413"  //can change    //variable  declaration
+accountCity ="jaipur"     //variable  declaration
+let accountState ="Not completed";  //;is optional
+//account_id=23   // changes not allowed as it is const
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+account_Email="Rishi@gmail.com"
+account_Password="21213"
+accountCity ="Bangalore"
+console.log(account_id);
+console.table([account_id,account_Email,account_Password,accountCity,accountState])
+
+//for declaring constant we have to use only "const" keyword
+// variables canbe declared by 2 methods ...1.let  2.var
