a9db01303877c275964b65b38b268887a986f1d6
rishita das <er.rishitadas@gmail.com>
2025-02-15 12:53:59 +0530

    Conversion

---
+++ datatype_conversion_2.js
@@ -0,0 +1,23 @@
+//let score =33
+//let valueinNumber =Number(score)
+
+
+let score= "33abc"
+//"33abc" = NaN (not a number) {conversion not possible}
+//true = boolean
+// 33 = number
+// "33" =string
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueinnumber =Number(score)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsloggedIn);
