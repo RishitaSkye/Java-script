42426bacabea281794dc97fb9d7f5e6b57cec99a
rishita das <er.rishitadas@gmail.com>
2025-02-21 22:07:03 +0530

    Conversation_Operation

--- datatype_conversion_1.js
+++ datatype_conversion_1.js
@@ -13,3 +13,15 @@ console.log(valueinnumber);
 // "33" => 33
 //"33abc" => NaN
 // true => 1 ; false => 0
+let isLoggedIn=""
+
+let booleanisloggedIn = Boolean(isLoggedIn)
+console.log(booleanisloggedIn);
+//1=>true     0=>false
+//""=>False
+//"trish"=>true
+
+let someNumber =32
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
