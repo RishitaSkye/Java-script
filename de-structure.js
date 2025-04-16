e1f4065c88fdee8cca2e382ae2ddf886f6c61d3a
rishita das <er.rishitadas@gmail.com>
2025-04-16 21:39:04 +0530

    de-structure_JSON

---
+++ de-structure.js
@@ -0,0 +1,25 @@
+const course={
+    coursename: "Javascript in hindi",
+    price:"999",
+    courseDuration: "13 hrs",
+    courseInstructor:"hitesh"
+}
+
+const {courseInstructor: instructor} = course
+//console.log(courseInstructor);
+console.log(instructor);
+
+//.....same as...........
+// const {courseInstructor} = course   
+// console.log(courseInstructor);
+
+// {
+//     "name":"trish",
+//     "coursename":"Js",
+//     "price":"free"
+// }
+[
+    {},
+    {},
+    {}
+]
