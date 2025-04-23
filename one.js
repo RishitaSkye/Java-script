95643da024525f3bc68020a5aab6fd4482a530d1
rishita das <er.rishitadas@gmail.com>
2025-04-23 20:59:31 +0530

    One

---
+++ one.js
@@ -0,0 +1,41 @@
+//if
+//const isUserLoggedIn=true
+
+//if (2==="2"){    //true or false format
+//console.log("executed");
+
+//}
+
+// Operators.....<,>,<=,>=,==,!=,===(type check),!==
+//if (false){
+    //any code in between doesnot execute
+//}
+// const isUserLoggedIn =true
+// const temperature=51
+
+// if(temperature>50){
+//     console.log("greater than 50");
+// }else{
+// console.log("temperature is lesser than 50");
+// }
+
+// const score=600
+// if(score>300){
+//     var power="fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+// 
+
+const UserLoggedIn =true
+const debitcard=true
+const LoggedInfromGOOGLE=false
+const loggedinfromEmail=true
+if(UserLoggedIn && debitcard && 2==2){
+    console.log("allowed to buy course");
+}
+if(LoggedInfromGOOGLE || loggedinfromEmail){
+    console.log("User Logged In");
+    
+}
