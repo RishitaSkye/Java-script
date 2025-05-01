c515b11da30e583d105303151572a7b85998f7a6
rishita das <er.rishitadas@gmail.com>
2025-05-01 16:02:43 +0530

    Array

---
+++ iterations/five.js
@@ -0,0 +1,50 @@
+const coding=["js", "ruby", "pearl", "python", "java", "cpp", "c"]
+coding.forEach( function (val) {
+console.log(val);
+})
+
+console.log();
+
+coding.forEach( (item) => {
+    console.log(item);
+})
+
+console.log();
+
+function printMe(item) {
+    console.log(item);
+    
+}
+coding.forEach(printMe);
+
+console.log();
+
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr);  
+})
+
+console.log();
+
+const myCoding = [
+    {
+        languageName: "Javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "Cpp",
+        languageFileName: "c++"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+    console.log(item.languageFileName);
+    
+    
+})
+
+
